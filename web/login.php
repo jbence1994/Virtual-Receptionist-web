02f0f7bd16eaf6dae817b9d4bb959cefab2abb93
jbence1994 <33096954+jbence1994@users.noreply.github.com>
2019-02-01 00:49:10 +0100

    Update login.php

--- web/login.php
+++ web/login.php
@@ -4,6 +4,9 @@ session_start();
 require_once('../config/connect.php');
 
 if (isset(post('submit'))) {
-    $username = post('username');
+    $accomodationID = post('accomodationID');
     $password = post('password');
+
+    $sql = "SELECT accomodation.AccomodationID, accomodation.Password FROM accomodation WHERE accomodation.AccomodationID = '$accomodationID' AND accomodation.Password = '$password';";
+    $result = $connection->query($sql);
 }
