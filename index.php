86be914295a4144a4079987ed7785ac9a1a8f445
jbence1994 <33096954+jbence1994@users.noreply.github.com>
2019-01-31 23:02:30 +0100

    Reszponzív nyitólap beadandó importálása

--- index.php
+++ index.php
@@ -1,17 +1,41 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
+<html lang="hu">
     <head>
-        <meta charset="UTF-8">
-        <title></title>
+        <title>Bejelentkezés | Virtual Receptionist</title>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>        
+        <script src="js/jquery.min.js" type="text/javascript"></script>
+        <script src="js/bootstrap.min.js" type="text/javascript"></script>
+        <link rel="stylesheet" type="text/css" href="css/main.css"/>
+        <script src="js/main.js" type="text/javascript"></script>
+        <link rel="icon" href="img/favicon.png" type="image/x-icon"/>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <div class="container">
+            <div class="logo">
+                <img src="img/vr-logo.png" alt="logo"/>
+            </div>
+            <form>
+                <div class="form-group" id="login-box">
+                    <h3>Adminisztrációs felület</h3>
+                    <input type="text" name="accomodationID" id="accomodationID" class="form-control" placeholder="Szálláshely azonosító" required/>
+                    <input type="password" name="password" id="password" class="form-control" placeholder="Jelszó" required/>
+                    <input type="submit" name="login" id="login" class="btn btn-primary" value="Bejelentkezés"/>
+                    <span><input type="checkbox" value=""/>Maradjon bejelentkezve</span>
+                    <a id="forgotPassword" href="#" data-toggle="tooltip" data-placement="right" title="Itt megadhat újat!">
+                        <span>Elfelejtette jelszavát?</span>
+                    </a>
+                </div>
+            </form>
+            <div id="registration">
+                <a href="#" data-toggle="tooltip" data-placement="right" title="Itt megteheti!">
+                    <span>Még nem regisztrálta szálláshelyét?</span>
+                </a>
+            </div>
+        </div>
+        <footer class=" footer-copyright text-center py-3">
+            <span>&copy; 2019. Virtual Receptionist | Juhász Bence</span>
+        </footer>
     </body>
 </html>
