57315e970035c2c08b2c20efdd35de5451f51a92
jbence1994 <33096954+jbence1994@users.noreply.github.com>
2019-02-05 14:36:43 +0100

    Egységes design beállítása statikusan

--- web/registration.php
+++ web/registration.php
@@ -2,9 +2,18 @@
 <html lang="hu">
     <head>
         <meta charset="utf-8">
-        <title></title>
+        <title>Szálláshely adatainak beállítása | Virtual Receptionist</title>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css"/>        
+        <script src="../js/jquery-3.3.1.min.js" type="text/javascript"></script>
+        <script src="../js/bootstrap.min.js" type="text/javascript"></script>
+        <link rel="stylesheet" type="text/css" href="../css/main.css"/>
+        <script src="../js/main.js" type="text/javascript"></script>
+        <link rel="icon" href="../img/favicon.png" type="image/x-icon"/>
     </head>
     <body>
-
+        <?php require_once('../templates/navbar.html'); ?>
+        <?php require_once('../templates/footer.html'); ?>
     </body>
 </html>
