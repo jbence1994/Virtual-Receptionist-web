75fb32a29ae9a954b1d76edd1eadedf0a2f9d089
jbence1994 <33096954+jbence1994@users.noreply.github.com>
2019-02-05 14:19:03 +0100

    Update setaccomodation.php

--- web/setaccomodation.php
+++ web/setaccomodation.php
@@ -1,17 +1,28 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
+<html lang="hu">
     <head>
-        <meta charset="UTF-8">
-        <title></title>
+        <meta charset="utf-8">
+        <title>Szálláshely adatainak beállítása | Virtual Receptionist</title>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css"/>        
+        <script src="../js/jquery-3.3.1.min.js" type="text/javascript"></script>
+        <script src="../js/bootstrap.min.js" type="text/javascript"></script>
+        <link rel="stylesheet" type="text/css" href="../css/main.css"/>
+        <script src="../js/main.js" type="text/javascript"></script>
+        <link rel="icon" href="../img/favicon.png" type="image/x-icon"/>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <?php require_once('../templates/navbar.html'); ?>
+        <input type="text" name="accomodationID" id="accomodationID" class="form-control" placeholder="Szálláshely azonosító" required/>
+        <input type="text" name="accomodationID" id="accomodationID" class="form-control" placeholder="Szálláshely azonosító" required/>
+        <input type="text" name="accomodationID" id="accomodationID" class="form-control" placeholder="Szálláshely azonosító" required/>
+        <input type="text" name="accomodationID" id="accomodationID" class="form-control" placeholder="Szálláshely azonosító" required/>
+        <input type="text" name="accomodationID" id="accomodationID" class="form-control" placeholder="Szálláshely azonosító" required/>
+        <input type="text" name="accomodationID" id="accomodationID" class="form-control" placeholder="Szálláshely azonosító" required/>
+        <input type="text" name="accomodationID" id="accomodationID" class="form-control" placeholder="Szálláshely azonosító" required/>
+        <input type="text" name="accomodationID" id="accomodationID" class="form-control" placeholder="Szálláshely azonosító" required/>
+        <input type="text" name="accomodationID" id="accomodationID" class="form-control" placeholder="Szálláshely azonosító" required/>
+        <?php require_once('../templates/footer.html'); ?>
     </body>
 </html>
