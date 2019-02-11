5e66cff38a33f5d31c04a0b775ef408a6864a8d2
Bence Juhasz <33096954+jbence1994@users.noreply.github.com>
2019-02-12 00:54:22 +0100

    Update select_room.php

--- CRUD/select_room.php
+++ CRUD/select_room.php
@@ -20,22 +20,23 @@ $rooms = "<table id='room_table'>"
 
 while ($row = $result->fetch_assoc()) {
     $rooms .= "<tr>"
-            . "<td>{$row['ID']}</td>"
-            . "<td>{$row['Name']}</td>"
-            . "<td>{$row['Number']}</td>"
-            . "<td>{$row['CategoryName']}</td>"
-            . "<td>{$row['Capacity']}</td>"
+            . "<td contenteditable>{$row['ID']}</td>"
+            . "<td contenteditable>{$row['Name']}</td>"
+            . "<td contenteditable>{$row['Number']}</td>"
+            . "<td contenteditable>{$row['CategoryName']}</td>"
+            . "<td contenteditable>{$row['Capacity']}</td>"
             . "<td><button class='btn btn-primary' class='delete' id='{$row['ID']}'>Törlés</button></td>"
+            . "<td><button class='btn btn-primary' class='update' id='{$row['ID']}'>Módisítás</button></td>"
             . "</tr>";
 }
 echo $rooms;
 
 $rooms = "<tr>"
-        . "<td id='ID'></td>"
-        . "<td id='name' contenteditable></td>"
-        . "<td id='number' contenteditable></td>"
-        . "<td id='category' contenteditable></td>"
-        . "<td id='capacity' contenteditable></td>"
+        . "<td class='editable' id='ID'></td>"
+        . "<td class='editable' id='name' contenteditable></td>"
+        . "<td class='editable' id='number' contenteditable></td>"
+        . "<td class='editable' id='category' contenteditable></td>"
+        . "<td class='editable' id='capacity' contenteditable></td>"
         . "<td><button class='btn btn-primary' class='insert'>Hozzáadás</button></td>"
         . "</tr>";
 $rooms .= "</table>";
