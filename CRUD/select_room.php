3a1b93352a46a928c688b26355f62443f04b6980
Bence Juhasz <33096954+jbence1994@users.noreply.github.com>
2019-02-11 23:28:06 +0100

    Update select_room.php

--- CRUD/select_room.php
+++ CRUD/select_room.php
@@ -1,8 +1,42 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+require_once('../config/connect.php');
 
+$sql = "SELECT * FROM room";
+$result = $connection->query($sql);
+
+if (!$result) {
+    die("Hiba az olvasás közben");
+}
+
+$rooms = "<table>"
+        . "<tr>"
+        . "<th>ID</th>"
+        . "<th>Szoba neve</th>"
+        . "<th>Szobaszám</th>"
+        . "<th>Kategória</th>"
+        . "<th>Kapacitás</th>"
+        . "</tr>";
+
+while ($row = $result->fet_assoc()) {
+    $rooms .= "<tr>"
+            . "<td>{$row['ID']}</td>"
+            . "<td>{$row['Name']}</td>"
+            . "<td>{$row['Number']}</td>"
+            . "<td>{$row['Category']}</td>"
+            . "<td>{$row['Capacity']}</td>"
+            . "<td>";
+}
+echo $rooms;
+
+$rooms = "<tr>"
+        . "<td id='ID'></td>"
+        . "<td id='name' conteneditable></td>"
+        . "<td id='number' conteneditable></td>"
+        . "<td id='category' conteneditable></td>"
+        . "<td id='capacity' conteneditable></td>"
+        . "</tr>";
+$rooms .= "</table>";
+echo $rooms;
+
+$connection->close();
