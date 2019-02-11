0181125b7344bf468c4b6b87713801a96977cd08
Bence Juhasz <33096954+jbence1994@users.noreply.github.com>
2019-02-11 23:10:50 +0100

    Update logged_out.php

--- web/logged_out.php
+++ web/logged_out.php
@@ -3,8 +3,18 @@
     <head>
         <meta charset="utf-8"/>
         <title>Hiba | Virtual Receptionist</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css"/>        
+        <script src="../js/jquery-3.3.1.min.js" type="text/javascript"></script>
+        <script src="../js/bootstrap.min.js" type="text/javascript"></script>
+        <link rel="stylesheet" type="text/css" href="../css/main.css"/>
+        <script src="../js/main.js" type="text/javascript"></script>
+        <link rel="icon" href="../img/favicon.png" type="image/x-icon"/>
     </head>
     <body>
-        <?php require_once('../templates/logged_out.html'); ?>
+        <div id="box">
+            <?php require_once('../templates/logged_out.html'); ?>
+        </div>
+        <?php require_once('../templates/footer.html'); ?>
     </body>
 </html>
