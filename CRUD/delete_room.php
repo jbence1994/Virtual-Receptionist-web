904b740a5792418349bb0534cf398e11fc6e3da6
Bence Juhasz <33096954+jbence1994@users.noreply.github.com>
2019-02-14 00:38:31 +0100

    Update delete_room.php

--- CRUD/delete_room.php
+++ CRUD/delete_room.php
@@ -1,8 +1,16 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+require_once('../config/connect.php');
 
+if (isset($_POST['del'])) {
+    $id = $_POST['del'];
+    $sql = "DELETE FROM room WHERE ID = '$id' ";
+
+    $result = $connection->query($sql);
+
+    if (!$result) {
+        die('Hiba a törlés során!');
+    }
+}
+
+$connection->close();
