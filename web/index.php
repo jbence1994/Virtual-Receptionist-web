01f65640bd6a7cfd2bdfd1e8f02726a4b89da4aa
Bence Juhasz <33096954+jbence1994@users.noreply.github.com>
2019-02-13 22:33:18 +0100

    <head> külön .html állományba mentése és PHP importálás a weblapokon.

--- web/index.php
+++ web/index.php
@@ -3,13 +3,7 @@
     <head>
         <meta charset="utf-8"/>
         <title>Bejelentkezés | Virtual Receptionist</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css"/>        
-        <script src="../js/jquery-3.3.1.min.js" type="text/javascript"></script>
-        <script src="../js/bootstrap.min.js" type="text/javascript"></script>
-        <link rel="stylesheet" type="text/css" href="../css/main.css"/>
-        <script src="../js/main.js" type="text/javascript"></script>
-        <link rel="icon" href="../img/favicon.png" type="image/x-icon"/>
+        <?php require_once('../templates/head.html'); ?>
     </head>
     <body>
         <div class="container">
--- web/mainmenu.php
+++ web/mainmenu.php
@@ -2,15 +2,8 @@
 <!DOCTYPE html>
 <html lang="hu">
     <head>
-        <meta charset="utf-8"/>
         <title>Főmenü | Virtual Receptionist</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css"/>        
-        <script src="../js/jquery-3.3.1.min.js" type="text/javascript"></script>
-        <script src="../js/bootstrap.min.js" type="text/javascript"></script>
-        <link rel="stylesheet" type="text/css" href="../css/main.css"/>
-        <script src="../js/main.js" type="text/javascript"></script>
-        <link rel="icon" href="../img/favicon.png" type="image/x-icon"/>
+        <?php require_once('../templates/head.html'); ?>
     </head>
     <body>
         <?php require_once('../templates/navbar.html'); ?>
