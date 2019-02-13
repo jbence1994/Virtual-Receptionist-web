055e18b8416aff880c74acb29ea7c150233a6244
Bence Juhasz <33096954+jbence1994@users.noreply.github.com>
2019-02-13 23:18:13 +0100

    <title> bugfix.

--- web/setbilling.php
+++ web/setbilling.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html lang="hu">
     <head>
-        <title>Szálláshely adatainak beállítása | Virtual Receptionist</title>
+        <title>Számlázási tételek beállítása | Virtual Receptionist</title>
         <?php require_once('../templates/head.html'); ?>
     </head>
     <body>
