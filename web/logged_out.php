ddcb93f106b2160282d42ad2abdada4a130832f5
Bence Juhasz <33096954+jbence1994@users.noreply.github.com>
2019-02-14 17:03:00 +0100

    Update logged_out.php

--- web/logged_out.php
+++ web/logged_out.php
@@ -5,8 +5,10 @@
         <?php require_once('../templates/head.html'); ?>
     </head>
     <body>
-        <div id="box">
-            <?php require_once('../templates/logged_out.html'); ?>
+        <div class="content">
+            <div id="box">
+                <?php require_once('../templates/logged_out.html'); ?>
+            </div>
         </div>
         <?php require_once('../templates/footer.html'); ?>
     </body>
