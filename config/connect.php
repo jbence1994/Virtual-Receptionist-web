d5b8b980e066b78efcbd61a9c127c616ef24f925
Bence Juhasz <33096954+jbence1994@users.noreply.github.com>
2019-02-14 17:00:40 +0100

    Kapcsolatleíró javítása.

--- config/connect.php
+++ config/connect.php
@@ -3,9 +3,9 @@
 $connection = new mysqli('localhost', 'root', '', 'virtual_receptionist', '3306');
 
 if ($connection->connect_errno) {
-    die('Nem sikerült csatlakozni!');
+    die('Nem sikerült csatlakozni!\n' . $connection_error);
 }
 
 if (!$connection->set_charset("utf8")) {
-    echo 'Nem sikerült beállítani a karakterkódolást!';
+    echo 'Nem sikerült beállítani a karakterkódolást!\n' . $connection->error;
 }
