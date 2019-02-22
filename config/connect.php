8d9e43aa3d4bd86b130f9478889f064465c22de2
Bence Juhasz <33096954+jbence1994@users.noreply.github.com>
2019-02-22 22:24:26 +0100

    Update connect.php

--- config/connect.php
+++ config/connect.php
@@ -6,6 +6,6 @@ if ($connection->connect_errno) {
     die('Nem sikerült csatlakozni!' . $connection_error);
 }
 
-if (!$connection->set_charset("utf8")) {
+if (!$connection->set_charset('utf8')) {
     echo 'Nem sikerült beállítani a karakterkódolást!' . $connection->error;
 }
