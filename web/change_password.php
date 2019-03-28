580b3f8e87d7c786018f1640087e731196adbca3
Bence Juhasz <33096954+jbence1994@users.noreply.github.com>
2019-03-28 16:01:01 +0100

    javítások.

--- web/change_password.php
+++ web/change_password.php
@@ -5,7 +5,7 @@
         <?php require_once('../templates/head.html'); ?>
     </head>
     <body>
-
+        <h4>Fejlesztés alatt...</h4>
     </body>
     <?php require_once('../templates/footer.php'); ?>
 </html>
