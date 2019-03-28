3be88ab6e65f66a11b75f983bf6ea0e8c199a6c1
Bence Juhasz <33096954+jbence1994@users.noreply.github.com>
2019-03-28 02:34:30 +0100

    sql create bugfix.

--- ajax_urls/create_room.php
+++ ajax_urls/create_room.php
@@ -2,11 +2,20 @@
 
 require_once('../config/connect.php');
 
-$sql = "";
-$result = $connection->query($sql);
+if (!empty($_POST['name']) && !empty($_POST['number']) && !empty($_POST['capacity']) && !empty($_POST['category'])) {
 
-if (!$result) {
-    die("Hiba!");
+    $name = $_POST['name'];
+    $number = $_POST['number'];
+    $capacity = $_POST['capacity'];
+    $category = $_POST['category'];
+
+    $sql = "INSERT INTO room (Number, Name, Category, Capacity) "
+            . "VALUES ('$number','$name','$category', '$capacity');";
+    $result = $connection->query($sql);
+
+    if (!$result) {
+        die("Hiba!");
+    }
+
+    $connection->close();
 }
-
-$connection->close();
